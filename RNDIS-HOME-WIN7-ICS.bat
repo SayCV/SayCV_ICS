f5bd68da136dbb6ea0d2707aabfc307ab1c0561d
SayCV <sssxyc@163.com>
2014-01-06 22:36:20 +0800

    add Requesting administrative privileges... to batch.

--- RNDIS-HOME-WIN7-ICS.bat
+++ RNDIS-HOME-WIN7-ICS.bat
@@ -1,7 +1,33 @@
 @echo off
 cd /d %~dp0
 
+:: BatchGotAdmin  
+:-------------------------------------  
+REM  --> Check for permissions  
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"  
+  
+REM --> If error flag set, we do not have admin.  
+if '%errorlevel%' NEQ '0' (  
+    echo Requesting administrative privileges...  
+    goto UACPrompt  
+) else ( goto gotAdmin )  
+  
+:UACPrompt  
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"  
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"  
+  
+    "%temp%\getadmin.vbs"  
+    exit /B  
+  
+:gotAdmin  
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )  
+    pushd "%CD%"  
+    CD /D "%~dp0"  
+:--------------------------------------  
+
 rem netsh wlan stop hostednetwork
 cscript /nologo ics.vbs "本地连接 2" "无线网络连接" "off"
 cscript /nologo ics.vbs "本地连接 2" "无线网络连接" "on"
 rem netsh wlan start hostednetwork
+
+pause
