464383e34f27b88db481acf9cd7382ff88241b65
SayCV <sssxyc@163.com>
2014-01-26 20:04:58 +0800

    update  to nexus 5 ....

--- RNDIS-HOME-WIN7-ICS.bat
+++ RNDIS-HOME-WIN7-ICS.bat
@@ -26,8 +26,8 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------  
 
 rem netsh wlan stop hostednetwork
-cscript /nologo ics.vbs "本地连接 2" "无线网络连接" "off"
-cscript /nologo ics.vbs "本地连接 2" "无线网络连接" "on"
+cscript /nologo ics.vbs "本地连接 7" "无线网络连接" "off"
+cscript /nologo ics.vbs "本地连接 7" "无线网络连接" "on"
 rem netsh wlan start hostednetwork
 
 pause
